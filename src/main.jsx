b4d3b62f0fe12abd0821fb9693ae52f4cb471b95
alimustafa905000-rgb <alimustafa905000@gmail.com>
2026-07-24 01:44:51 +0500

    style: update global styles import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./styles/global.css";
+// import "./components/glass.css";
+
+import App from "./App.jsx";
+
+import AdminAuthProvider from "./admin/context/AdminAuthContext";
+import AdminThemeProvider from "./admin/context/AdminThemeContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AdminThemeProvider>
+        <AdminAuthProvider>
+          <App />
+          <Toaster position="top-right" />
+        </AdminAuthProvider>
+      </AdminThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
